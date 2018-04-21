bc972fd0fd473c42522e073533911492cf738cde
ahmed youssef elsayed <ahmedyoussef59@yahoo.com>
2018-04-21 14:16:21 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,40 +1,33 @@
-$(document).ready(function(){
-  getIt()
-  frameIt()
-});
- 	 
-
-{
- $('p').on('click', function()
-  {
+function getIt() {
+  $('p').on('click', function() {
     alert('Hey!')
   })
 }
  	 
-});	
-function frameIt()
-{
-  $('img').on('load', function()
-  {
-    $('img').addClass('tasty')
+function frameIt () {
+  $('img').on('load', function () {
+    $('img').addClass('tasty');
   })
-}
 
-function pressIt()
-{
-  $('form input').on('keydown', function(key)
-  {
-    if(key.which === 71)
-    {
-      alert('You pressed the "G" key.')
+ 	 
+}
+function pressIt() {
+  $('form').on('keydown', function(event) {
+    if (event.which == 71) {
+      alert('g was pressed')
     }
   })
 }
-
-function submitIt()
-{
-  $('form').on('submit', function()
-  {
+function submitIt() {
+  $('form').on('submit', function() {
     alert('Your form is going to be submitted now.')
   })
-} 
+}
+
+$(document).ready(function() {
+
+getIt() 
+frameIt()
+pressit()
+submitIt()
+})
