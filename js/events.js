874b7d16ab9086bd76d61145b83e606dc53232b3
ahmed youssef elsayed <ahmedyoussef59@yahoo.com>
2018-04-21 14:11:12 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
-//define functions here
-
 $(document).ready(function(){
-
-// call functions here
-
+  getIt()
+  frameIt()
 });
+ 	 
+
+{
+ $('p').on('click', function()
+  {
+    alert('Hey!')
+  })
+}
+ 	 
+});	
+function frameIt()
+{
+  $('img').on('load', function()
+  {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt()
+{
+  $('form input').on('keydown', function(key)
+  {
+    if(key.which === 71)
+    {
+      alert('You pressed the "G" key.')
+    }
+  })
+}
+
+function submitIt()
+{
+  $('form').on('submit', function()
+  {
+    alert('Your form is going to be submitted now.')
+  })
+} 
